f84410dd952f3afefca77e96156439c3e5f9066a
Purinat33 <Purinat33@gmail.com>
2023-01-20 23:01:15 +0700

    more progress on

--- scripts/app.js
+++ scripts/app.js
@@ -1,1 +1,12 @@
 //This is for search function
+
+const search = () =>{
+    const searchbox = document.getElementById("search-item").ariaValueMax.toUpperCase();
+    const storeitems = document.getElementById("product-list");
+    const product = document.querySelectorAll("product"); 
+    //If each button is pressed
+    //h2 = name
+    //h3 = genre
+    //h4 = price
+    const pname = document.getElementsByTagName("h2");
+};
