4067636bb59836f8524fd708308252bd56add52a
Purinat33 <Purinat33@gmail.com>
2023-01-20 15:36:55 +0700

    start search functionality

--- components/header.js
+++ components/header.js
@@ -24,7 +24,12 @@ class Header extends HTMLElement{
             padding: 0;
             margin: 0;
             box-sizing: border-box;
+        }
 
+        body{
+            width: 100%;
+            height: 100vh;
+            background: rgb(240,239,243);
         }
 
         .credit{
---
+++ scripts/app.js
@@ -0,0 +1,1 @@
+//This is for search function
